f55d4f9ecc6e33cdc951dca768afbae2dd97926c
Brahian Calo Suhr <brahian.calo@mercadolibre.com>
2023-01-31 18:22:58 -0300

    finish base calculator imp

--- calculator/calculator.ts
+++ calculator/calculator.ts
@@ -1,5 +1,17 @@
 export default class Calculator {
-  sum(a: number, b: number) {
+  sum(a: number, b: number): number {
     return a + b;
+  }
+
+  subtract(a: number, b: number): number {
+    return a - b;
+  }
+
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+
+  divide(a: number, b: number): number {
+    return a / b;
   }
 }
