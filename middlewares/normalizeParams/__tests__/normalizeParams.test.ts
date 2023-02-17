2b712748bc1009f08bae9a2acd35957a23cdccd5
Brahian Calo Suhr <brahian.calo@mercadolibre.com>
2023-02-17 19:09:46 -0300

    Add more tests

--- middlewares/normalizeParams/__tests__/normalizeParams.test.ts
+++ middlewares/normalizeParams/__tests__/normalizeParams.test.ts
@@ -38,7 +38,7 @@ describe('normalizeQueryParams', () => {
   });
 
   it('should normalize a date query params', (done) => {
-    const testDate ='2023-02-17T20:14:52.422Z'
+    const testDate ='2023-02-17T20:14:52.422Z';
     const req: Request = {
       query: {
         last_modification: testDate,
@@ -68,12 +68,47 @@ describe('normalizeQueryParams', () => {
   it('should normalize boolean query params', (done) => {
     const req: Request = {
       query: {
-        is_admin: 'true',
+        is_admin: 'TRUE',
       }
     };
 
     normalizer(req, res, () => {
       expect(res.locals).toHaveProperty('is_admin', true);
+      done();
+    });
+  });
+
+  it('should leave unmapped query params as strings', (done) => {
+    const testString = 'not defined';
+    const req: Request = {
+      query: {
+        not_present: testString
+      }
+    };
+
+    normalizer(req, res, () => {
+      expect(res.locals).toHaveProperty('not_present', testString);
+      done();
+    });
+  });
+
+  it('should transform multiple params', (done) => {
+    const testDate ='2022-06-17';
+    const testString = 'value';
+    const req: Request = {
+      query: {
+        limit: '30',
+        last_modification: testDate,
+        is_admin: 'false',
+        random: testString
+      }
+    };
+
+    normalizer(req, res, () => {
+      expect(res.locals).toHaveProperty('limit', 30);
+      expect(res.locals).toHaveProperty('is_admin', false);
+      expect(res.locals).toHaveProperty('random', testString);
+      expect(res.locals.last_modification!.getTime()).toBe(new Date(testDate).getTime());
       done();
     });
   });
