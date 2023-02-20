5e0fe4e3dd6f97fd57f39fcae21c3a3f7d35b082
Brahian Calo Suhr <brahian.calo@mercadolibre.com>
2023-02-20 12:17:38 -0300

    setup unfinished tests

---
+++ calculator/__tests__/calculator.test.ts
@@ -0,0 +1,37 @@
+import Calculator from "..";
+
+describe('Calculator', () => {
+  let calculator: Calculator;
+
+  beforeEach(() => {
+    calculator = new Calculator();
+  });
+
+  describe('sum', () => {
+    it('should sum two numbers', () => {
+      const result = calculator.sum(2, 8);
+      expect(result).toBe(10);
+    });
+  });
+
+  describe('subtract', () => {
+    it('should subtract two numbers', () => {
+      const result = calculator.subtract(10, 8);
+      expect(result).toBe(2);
+    });
+  });
+
+  describe('multiply', () => {
+    it('should multiply two numbers', () => {
+      const result = calculator.multiply(2, 8);
+      expect(result).toBe(16);
+    });
+  });
+
+  describe('divide', () => {
+    it('should divide two numbers', () => {
+      const result = calculator.divide(10, 5);
+      expect(result).toBe(2);
+    });
+  });
+});
--- middlewares/normalizeParams/__tests__/normalizeParams.test.ts
+++ middlewares/normalizeParams/__tests__/normalizeParams.test.ts
@@ -1,12 +1,13 @@
 import { normalizeQueryParams, RequestHandler, ParamsType, Request, Response } from "..";
 
-describe('normalizeQueryParams', () => {
+describe('normalizeParams', () => {
   let normalizer: RequestHandler;
   let res: Response<{
     limit?: number,
     last_modification?: Date,
     type?: string,
-    is_admin?: boolean
+    is_admin?: boolean,
+    unrelatedProp?: string;
   }>;
 
   beforeAll(() => {
@@ -22,59 +23,81 @@ describe('normalizeQueryParams', () => {
     res = {
       locals: {},
     };
-  })
-
-  it('should normalize a numeric query params', (done) => {
-    const req: Request = {
-      query: {
-        limit: '20',
-      }
-    };
-
-    normalizer(req, res, () => {
-      expect(res.locals).toHaveProperty('limit', 20);
-      done();
-    })
   });
 
-  it('should normalize a date query params', (done) => {
-    const testDate ='2023-02-17T20:14:52.422Z';
-    const req: Request = {
-      query: {
-        last_modification: testDate,
-      }
-    };
+  describe('number', () => {
+    it('should normalize a numeric query params', (done) => {
+      const req: Request = {
+        query: {
+          limit: '20',
+        }
+      };
 
-    normalizer(req, res, () => {
-      expect(res.locals.last_modification!.getTime()).toBe(new Date(testDate).getTime());
-      done();
-    })
+      normalizer(req, res, () => {
+        expect(res.locals).toHaveProperty('limit', 20);
+        done();
+      })
+    });
+
+    it('should normalize a numeric query params as zero if the value is not numeric', (done) => {
+      const req: Request = {
+        query: {
+          limit: 'im not numeric',
+        }
+      };
+
+      normalizer(req, res, () => {
+        expect(res.locals).toHaveProperty('limit', 0);
+        done();
+      });
+    });
+
   });
 
-  it('should leave string query params and just pass them to res.locals', (done) => {
-    const testType = 'widget';
-    const req: Request = {
-      query: {
-        type: testType,
-      }
-    };
+  describe('date', () => {
+    it('should normalize a date query params', (done) => {
+      const testDate ='2023-02-17T20:14:52.422Z';
+      const req: Request = {
+        query: {
+          last_modification: testDate,
+        }
+      };
 
-    normalizer(req, res, () => {
-      expect(res.locals).toHaveProperty('type', testType);
-      done();
+      normalizer(req, res, () => {
+        expect(res.locals.last_modification!.getTime()).toBe(new Date(testDate).getTime());
+        done();
+      })
     });
   });
 
-  it('should normalize boolean query params', (done) => {
-    const req: Request = {
-      query: {
-        is_admin: 'TRUE',
-      }
-    };
+  describe('string', () => {
+    it('should leave string query params unchanged pass them to res.locals', (done) => {
+      const testType = 'widget';
+      const req: Request = {
+        query: {
+          type: testType,
+        }
+      };
 
-    normalizer(req, res, () => {
-      expect(res.locals).toHaveProperty('is_admin', true);
-      done();
+      normalizer(req, res, () => {
+        expect(res.locals).toHaveProperty('type', testType);
+        done();
+      });
+    });
+  });
+
+  describe('boolean', () => {
+    it('should normalize boolean query params', (done) => {
+      const req: Request = {
+        query: {
+          is_admin: 'TRUE',
+        }
+      };
+
+      normalizer(req, res, () => {
+        expect(res.locals).toHaveProperty('is_admin', true);
+        done();
+      });
     });
   });
 
@@ -109,6 +132,24 @@ describe('normalizeQueryParams', () => {
       expect(res.locals).toHaveProperty('is_admin', false);
       expect(res.locals).toHaveProperty('random', testString);
       expect(res.locals.last_modification!.getTime()).toBe(new Date(testDate).getTime());
+      done();
+    });
+  });
+
+  it('should keep unrelated and previously set values on locals', (done) => {
+    const expectedUnrelatedProp = 'I remain unchanged';
+    res.locals.unrelatedProp = expectedUnrelatedProp;
+    const req: Request = {
+      query: {
+        limit: '30',
+        is_admin: 'false',
+      }
+    };
+
+    normalizer(req, res, () => {
+      expect(res.locals).toHaveProperty('limit', 30);
+      expect(res.locals).toHaveProperty('is_admin', false);
+      expect(res.locals).toHaveProperty('unrelatedProp', expectedUnrelatedProp);
       done();
     });
   });
