bdbbfffdeacb836a93755298b5d02c12244f7e46
Brahian Calo Suhr <brahian.calo@mercadolibre.com>
2023-02-20 16:30:42 -0300

    add number type to debounce

--- debounce/index.ts
+++ debounce/index.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_TIMEOUT = 200;
 
 export default function debounce(callback: Function, timeout = DEFAULT_TIMEOUT) {
-  let timer;
+  let timer: number;
   return () => {
     clearTimeout(timer);
     timer = setTimeout(callback, timeout);
